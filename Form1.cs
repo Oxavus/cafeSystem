25d01261e8818d4ab2dda7872006c6d15b1cf725
Oxavus <oxavus@gmail.com>
2024-07-21 03:56:09 +0700

    first commit

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using caffeeSystem.SubForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caffeeSystem
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           ADDFORM Addform = new ADDFORM();
+            Addform.Show();
+        }
+    }
+}
---
+++ SubForm/ADDFORM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caffeeSystem.SubForm
+{
+   internal partial class ADDFORM : Form
+    {
+        public ADDFORM()
+        {
+            InitializeComponent();
+        }
+
+        private void ADDFORM_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
